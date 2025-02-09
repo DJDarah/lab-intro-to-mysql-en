c62616b8cbdde363bc82e048170097ad961507ca
DaraH <sombeinar@gmail.com>
2025-02-09 01:02:11 -0400

    Delete seeding.sql

--- seeding.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Cars (VIN, Manufacturer, Model, Year, Color) VALUES
-('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
-('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
-('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White');
